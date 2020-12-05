61955ec954b36c1668d1f1ea5f2ea5c5fe831fe7
Mike S Chen <mike.sihong.chen@gmail.com>
2020-12-05 11:34:39 -0500

    react setup and firebase app setup

---
+++ client/src/Firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAclnJjNWwAr0IW9Lkd2VMUPZnlG8uZx7w",
+    authDomain: "quiz-galaxy.firebaseapp.com",
+    projectId: "quiz-galaxy",
+    storageBucket: "quiz-galaxy.appspot.com",
+    messagingSenderId: "1066396990067",
+    appId: "1:1066396990067:web:820f8d3fd9d205025e0806",
+    measurementId: "G-9T49NN5970",
+};
+
+firebase.initializeapp(firebaseConfig);
